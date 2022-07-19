0ff4dd58a74cd53bb6d29afa96319b8d9c3b2154
David López <dalopez@javeriana.edu.co>
2022-07-19 16:11:17 -0500

    Login front
    Add front for login and back refactor for res.error

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -3,3 +3,8 @@ const axios = require('axios');
 export const createUser = (user) => axios.post('http://localhost:8080/api/users/create',{
     user
 });
+
+export const login = (user) => axios.post('http://localhost:8080/api/users/login',{
+    email: user.email,
+    password: user.password
+})
