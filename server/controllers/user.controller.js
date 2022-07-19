979c01f84d707e52fe4e3d478bf8fd3d51f365ba
David López <dalopez@javeriana.edu.co>
2022-07-19 15:10:05 -0500

    Register communication with back

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,15 +2,22 @@ const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 
-module.exports.register = (req, res) => {
+// module.exports.register = (req, res) => {
 
-    const user = new User(req.body);
+//     const user = new User(req.body);
 
-    user
-        .save()
-        .then(() => {
-            res.json({ msg: "El usuario fue exitosamente registrado" });
-        })
-        .catch(err => res.status(500).json({ error: err, msg: "No se pudo registrar el usuario" }));
+//     user
+//         .save()
+//         .then(() => {
+//             res.json({ msg: "El usuario fue exitosamente registrado" });
+//         })
+//         .catch(err => res.status(500).json({ error: err, msg: "No se pudo registrar el usuario" }));
         
-};
+// };
+
+module.exports.register = (req, res) => {
+    console.log(req.body.user);
+    User.create(req.body.user)
+        .then(newUser => res.json({ newUser, msg: "El usuario fue exitosamente registrado" }))
+        .catch(err => res.status(500).json({ error: err, msg: 'No se pudo registrar el usuario' }));
+}
